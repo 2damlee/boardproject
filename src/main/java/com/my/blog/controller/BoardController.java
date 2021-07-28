8c885b782d042da2c9116f8248308084e0eb89cb
2damlee <2damlee@gmail.com>
2021-07-28 21:46:58 +0900

    crud 기본 골조 추가

--- src/main/java/com/my/blog/controller/BoardController.java
+++ src/main/java/com/my/blog/controller/BoardController.java
@@ -11,52 +11,59 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.my.blog.service.BoardServiceImpl;
 import com.my.blog.vo.BoardVO;
+import com.my.blog.vo.ReplyVO;
 
 
 @Controller
 public class BoardController {
-@Autowired
-BoardServiceImpl service;
+	@Autowired
+	BoardServiceImpl service;
+	
+	@RequestMapping("/home")
+	public String home() {
+		return "board/home";
+	}
+	
+	@RequestMapping("/boardlist")
+	public ModelAndView boardList() {
+		ModelAndView mv = new ModelAndView();
+		List<BoardVO> list = service.getAllList();
+		mv.addObject("list", list);
+		mv.setViewName("board/boardlist");
+		return mv;
+	}
+	
+	@RequestMapping("/boarddetail")
+	public ModelAndView boardDetail(int bno) {
+		ModelAndView mv = new ModelAndView();
+		List<BoardVO> detail = service.getDetail(bno);
+		mv.addObject("detail", detail);
+		List<ReplyVO> replyList = service.getReplyList(bno);
+		mv.addObject("replyList", replyList);
+		mv.setViewName("board/boarddetail");
 
-@RequestMapping("/home")
-public String home() {
-	return "board/home";
-}
-
-
-@RequestMapping("/boardlist")
-public ModelAndView boardList() {
-	ModelAndView mv = new ModelAndView();
-	List<BoardVO> list = service.getAllList();
-	mv.addObject("list", list);
-	mv.setViewName("board/boardlist");
-	return mv;
-}
-
-
-@RequestMapping("/boarddetail")
-public ModelAndView boardDetail(int bno) {
-	ModelAndView mv = new ModelAndView();
-	List<BoardVO> detail = service.getDetail(bno);
-	mv.addObject("detail", detail);
-	mv.setViewName("board/boarddetail");
-	return mv;
-}
-
-@RequestMapping(value="/boardwrite", method = RequestMethod.GET)
-public String writeBoardForm() {
-	return "/board/boardwrite";
-}
-
-@RequestMapping(value="/boardwirte", method = RequestMethod.POST)
-public void boardWrite(BoardVO vo) {
-	service.writeBoard(vo);
-} //boardwrite.jsp 만들어야 함  
-
-@RequestMapping(value="/chat")
-public String chat() {
-	return "/board/chat";
-} //boardwrite.jsp 만들어야 함  
-
+		return mv;
+	}
+	
+	@RequestMapping(value="/boardwrite", method = RequestMethod.GET)
+	public String writeBoardForm() {
+		return "/board/boardwrite";
+	}
+	
+	@RequestMapping(value="/boardwirte", method = RequestMethod.POST)
+	public void boardWrite(BoardVO vo) {
+		service.writeBoard(vo);
+	} //boardwrite.jsp 만들어야 함  
+	
+	@RequestMapping(value="/replywrite", method = RequestMethod.POST)
+	public void replyWrite(ReplyVO vo) {
+		service.saveReply(vo);
+	}
+	
+	@RequestMapping(value="/replydelete")
+	public void replyDelete(int rno) {
+		service.deleteBoard(rno);
+	}
+	
 
 }
